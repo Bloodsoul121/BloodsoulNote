21bda50d306fb99af2ddb43b5717c065b582dc46
caiguangzu <caiguangzu@gionee.com>
2017-11-01 11:35:06 +0800

    输入法辅助栏更新

--- app/src/main/java/com/gionee/bloodsoulnote/inputbar/InputBarActivity.java
+++ app/src/main/java/com/gionee/bloodsoulnote/inputbar/InputBarActivity.java
@@ -1,9 +1,14 @@
 package com.gionee.bloodsoulnote.inputbar;
 
 import android.app.Activity;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.graphics.Rect;
 import android.os.Bundle;
 import android.text.Editable;
+import android.util.Log;
 import android.view.View;
 import android.view.ViewTreeObserver.OnGlobalLayoutListener;
 import android.widget.EditText;
@@ -23,6 +28,8 @@ public class InputBarActivity extends Activity {
 
     private boolean mLastKeybordState = false; // true: show; false:hide
 
+    private int mLastVisibleFrameHeight;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -40,6 +47,7 @@ public class InputBarActivity extends Activity {
                 setShortcutInputText(text);
             }
         });
+        registerInputMethodChanged();
     }
 
     private void setShortcutInputText(String character) {
@@ -72,8 +80,12 @@ public class InputBarActivity extends Activity {
             int heightDiff = screenHeight - (rect.bottom - rect.top);
             // 如果输入法的高度大于屏幕的1/3, 那就表示输入法弹出来了
             boolean visible = Math.abs(heightDiff) > screenHeight / 3;
-            if (mLastKeybordState != visible) {
+            Log.i("InputBarActivity", "mLastKeybordState != visible : " + (mLastKeybordState != visible) + ", isInputMethodChanged : " + isInputMethodChanged
+                                        + ", visibleFrameHeight :" + visibleFrameHeight);
+            if (mLastKeybordState != visible || isInputMethodChanged || mLastVisibleFrameHeight!=visibleFrameHeight) {
                 mLastKeybordState = visible;
+                mLastVisibleFrameHeight = visibleFrameHeight;
+                isInputMethodChanged = false;
                 onKeyboardStateChanged(visible ? KEYBOARD_STATE_SHOW : KEYBOARD_STATE_HIDE, visibleFrameHeight);
             }
         }
@@ -118,6 +130,7 @@ public class InputBarActivity extends Activity {
     protected void onDestroy() {
         super.onDestroy();
         removeGlobalOnLayoutListener();
+        unregisterReceiver(mInputMethodReceiver);
     }
 
     private void removeGlobalOnLayoutListener() {
@@ -125,6 +138,27 @@ public class InputBarActivity extends Activity {
             mShortcutInputView.getViewTreeObserver().removeOnGlobalLayoutListener(mOnGlobalLayoutListener);
         } else {
             mShortcutInputView.getViewTreeObserver().removeGlobalOnLayoutListener(mOnGlobalLayoutListener);
+        }
+    }
+
+    // 注册输入法的切换监听
+    private InputMethodChangedReceiver mInputMethodReceiver;
+
+    private boolean isInputMethodChanged = false;
+
+    private void registerInputMethodChanged() {
+        mInputMethodReceiver = new InputMethodChangedReceiver();
+        IntentFilter intentFilter = new IntentFilter();
+        intentFilter.addAction(Intent.ACTION_INPUT_METHOD_CHANGED);
+        registerReceiver(mInputMethodReceiver, intentFilter);
+    }
+
+    private class InputMethodChangedReceiver extends BroadcastReceiver {
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            Log.i("InputBarActivity", "InputMethodChangedReceiver onReceive ");
+            isInputMethodChanged = true;
         }
     }
 
